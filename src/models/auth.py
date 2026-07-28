ee062d7514a601e03ddab27b5db3cb6fb926f209
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-07-28 21:19:43 +0530

    Added Auth, Organization Models

---
+++ src/models/auth.py
@@ -0,0 +1,44 @@
+import datetime
+import uuid
+from enum import Enum
+
+from sqlmodel import AutoString, Field, SQLModel
+
+
+class RefreshToken(SQLModel, table=True):
+    __tablename__ = "refresh_tokens"  # pyright: ignore[reportAssignmentType]
+
+    id: uuid.UUID = Field(
+        primary_key=True, default_factory=uuid.uuid4, index=True, nullable=False
+    )
+    user_id: uuid.UUID = Field(
+        nullable=False, foreign_key="users.id", ondelete="CASCADE"
+    )
+    token_hash: str = Field(nullable=False)
+    expires_at: datetime.datetime = Field(nullable=False)
+    revoked_at: datetime.datetime = Field(nullable=True)
+    created_at: datetime.datetime = Field(
+        nullable=False,
+        default_factory=lambda: datetime.datetime.now(datetime.timezone.utc),
+    )
+
+
+class TokenType(str, Enum):
+    EMAIL_VERIFY = "email_verify"
+    PASSWORD_RESET = "password_reset"
+
+
+class VerificationToken(SQLModel, table=True):
+    __tablename__ = "verification_tokens"  # pyright: ignore[reportAssignmentType]
+
+    id: uuid.UUID = Field(
+        primary_key=True, default_factory=uuid.uuid4, index=True, nullable=False
+    )
+    token_hash: str = Field(nullable=False)
+    type: TokenType = Field(sa_type=AutoString, nullable=False, index=True)
+    expires_at: datetime.datetime = Field(nullable=False)
+    used_at: datetime.datetime = Field(nullable=True)
+    created_at: datetime.datetime = Field(
+        nullable=False,
+        default_factory=lambda: datetime.datetime.now(datetime.timezone.utc),
+    )
---
+++ src/models/organization.py
@@ -0,0 +1,90 @@
+from datetime import datetime, timezone
+from enum import Enum
+
+from sqlmodel import AutoString, Field, SQLModel, UniqueConstraint
+from sqlmodel.main import uuid
+
+
+class Organization(SQLModel, table=True):
+    __tablename__ = "organizations"  # pyright: ignore[reportAssignmentType]
+
+    id: uuid.UUID = Field(
+        primary_key=True, default_factory=uuid.uuid4, index=True, nullable=False
+    )
+    name: str = Field(
+        nullable=False,
+    )
+    slug: str = Field(nullable=False, unique=True)
+    description: str = Field(nullable=True)
+    logo_url: str = Field(nullable=True)
+    created_by: uuid.UUID = Field(nullable=False, foreign_key="users.id")
+    created_at: datetime = Field(
+        nullable=False, default_factory=lambda: datetime.now(timezone.utc)
+    )
+    updated_at: datetime = Field(
+        nullable=False,
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)},
+    )
+
+
+class OrgRole(str, Enum):
+    OWNER = "owner"
+    ADMIN = "admin"
+    MEMBER = "member"
+
+
+class OrganizationMember(SQLModel, table=True):
+    __tablename__ = "organization_members"  # pyright: ignore[reportAssignmentType]
+
+    id: uuid.UUID = Field(
+        primary_key=True, default_factory=uuid.uuid4, index=True, nullable=False
+    )
+    organization_id: uuid.UUID = Field(
+        nullable=False, foreign_key="organizations.id", ondelete="CASCADE"
+    )
+    user_id: uuid.UUID = Field(
+        nullable=False, foreign_key="users.id", ondelete="CASCADE"
+    )
+    role: OrgRole = Field(
+        nullable=False,
+        default=OrgRole.MEMBER,
+        sa_type=AutoString,
+    )
+
+    joined_at: datetime = Field(
+        nullable=False, default_factory=lambda: datetime.now(timezone.utc)
+    )
+
+    __table_args__ = (
+        UniqueConstraint("organization_id", "user_id", name="uq_organization_user"),
+    )
+
+
+class InviteStatus(str, Enum):
+    PENDING = "pending"
+    ACCEPTED = "accepted"
+    EXPIRED = "expired"
+
+
+class OrganizationInvites(SQLModel, table=True):
+    __tablename__ = "organization_invites"  # pyright: ignore[reportAssignmentType]
+
+    id: uuid.UUID = Field(
+        primary_key=True, default_factory=uuid.uuid4, index=True, nullable=False
+    )
+
+    organization_id: uuid.UUID = Field(
+        nullable=False, foreign_key="organizations.id", ondelete="CASCADE"
+    )
+    email: str = Field(nullable=False)
+    role: OrgRole = Field(nullable=False, default=OrgRole.MEMBER, sa_type=AutoString)
+    invited_by: uuid.UUID = Field(nullable=False, foreign_key="users.id")
+    token_hash: str = Field(nullable=False)
+    staus: InviteStatus = Field(
+        nullable=False, default=InviteStatus.PENDING, sa_type=InviteStatus
+    )
+    expires_at: datetime = Field(nullable=False)
+    created_at: datetime = Field(
+        nullable=False, default_factory=lambda: datetime.now(timezone.utc)
+    )
